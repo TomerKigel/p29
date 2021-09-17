1865dfb68a434c5c23157eb2169329d04ae2fac1
Tomer Kigel <88850793+TomerKigel@users.noreply.github.com>
2021-09-17 20:35:18 +0300

    Update p29.java

--- p29.java
+++ p29.java
@@ -1,75 +1,61 @@
-import java.util.*;
+public class p29 {
+		public static void main(String[] args) {
+			int count = 99*99;
+			Vector<Integer> Inputs = new Vector<Integer>();
+			Vector<Integer> outputs = new Vector<Integer>();
 
-public class p61 {
-	public static void main(String[] args) {
-		Vector<Integer> Inputs = new Vector<Integer>();
-		Vector<Integer> outputs = new Vector<Integer>();
-		
-		Inputs.add(100);
-		outputs.add(0);
-		
-		Inputs.add(5000);
-		outputs.add(145);
-		
-		Inputs.add(10000);
-		outputs.add(145);
-		
-		Inputs.add(100000);
-		outputs.add(40730);
-		
-		Test(Inputs,outputs);
-		System.out.println("Answer is: " + p33(100000));
-	}
+			Inputs.add(10);
+			outputs.add(88);
 
-	public static long p33(long bound)
-	{
-		long [] f_numbers = new long[10];
-		long total = 0;
-		digit_factor_worth(f_numbers);
-		for (long i = 3; i < bound; i++)
+			Inputs.add(20);
+			outputs.add(361);
+
+			Inputs.add(30);
+			outputs.add(814);
+
+			Inputs.add(40);
+			outputs.add(1444);
+
+			Inputs.add(50);
+			outputs.add(2279);
+
+			Inputs.add(60);
+			outputs.add(3335);
+
+			Test(Inputs,outputs);
+			System.out.println("Answer is: " + p29(99));
+
+		}
+		public static int p29(int bound)
 		{
-			long sum = 0;
-			long t_num = i;
-			while(t_num/10 >= 1)
-			{
-				sum += f_numbers[(int)(t_num % 10)];
-				t_num /= 10;
-			}
-			sum += f_numbers[(int)(t_num % 10)];
+			int count = bound*bound;
+			TreeSet set = new  TreeSet();
 
-
-			if(sum == i)
-			{
-				total += sum;
-			}
+			for (int a = 2; a <= 100; a++) {
+			    for (int b = 2; b <= 100; b++) {
+			    	if(a == 99 & b == 99)
+			    		System.out.println();
+			for (int a = 2; a <= bound+1; a++) {
+			    for (int b = 2; b <= bound+1; b++) {
+			        double result = Math.pow(a, b);
+			        set.add(result);
+			     }
+			 }
+			System.out.println(set.size());
+	}
+			return set.size();
 		}
-		return total;
-	}
-	public static int factor(int num)
-	{
-		if(num <= 1)
-			return 1;
-		return factor(num-1) * num;
-	}
-	static long [] digit_factor_worth(long f_numbers[])
-	{
-		for (int i = 0; i < 10; i++)
+
+		public static void Test(Vector<Integer> Inputs,Vector<Integer> Excpected_Outputs)
 		{
-			f_numbers[i] = factor(i);
+			int i = 1;
+			for (Integer num : Inputs) {
+				System.out.print("Test number" + i +":");
+				if(p29(num) == Excpected_Outputs.elementAt(i-1))
+					System.out.println("Passed");
+				else
+					System.out.println("Failed");
+				i++;
+			}
 		}
-		return f_numbers;
-	}
-	
-	public static void Test(Vector<Integer> Inputs,Vector<Integer> Excpected_Outputs)
-	{
-		int i = 1;
-		for (Integer num : Inputs) {
-			System.out.print("Test number" + i +":");
-			if(p33(num) == Excpected_Outputs.elementAt(i-1))
-				System.out.println("Passed");
-			else
-				System.out.println("Failed");
-			i++;
-		}
-	}
 }
