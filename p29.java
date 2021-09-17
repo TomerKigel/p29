1ddb8111c6304b2af8e013451cbc3e1c95b6b350
Tomer Kigel <88850793+TomerKigel@users.noreply.github.com>
2021-09-17 20:41:49 +0300

    Update p29.java

--- p29.java
+++ p29.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class p29 {
 		public static void main(String[] args) {
 			int count = 99*99;
@@ -5,44 +7,36 @@ public class p29 {
 			Vector<Integer> outputs = new Vector<Integer>();
 
 			Inputs.add(10);
-			outputs.add(88);
+			outputs.add(69);
 
 			Inputs.add(20);
-			outputs.add(361);
+			outputs.add(324);
 
 			Inputs.add(30);
-			outputs.add(814);
+			outputs.add(755);
 
 			Inputs.add(40);
-			outputs.add(1444);
+			outputs.add(1365);
 
 			Inputs.add(50);
-			outputs.add(2279);
+			outputs.add(2184);
 
 			Inputs.add(60);
-			outputs.add(3335);
+			outputs.add(3216);
 
 			Test(Inputs,outputs);
-			System.out.println("Answer is: " + p29(99));
+			System.out.println("Answer is: " + p29(100));
 
 		}
 		public static int p29(int bound)
 		{
-			int count = bound*bound;
 			TreeSet set = new  TreeSet();
-
-			for (int a = 2; a <= 100; a++) {
-			    for (int b = 2; b <= 100; b++) {
-			    	if(a == 99 & b == 99)
-			    		System.out.println();
-			for (int a = 2; a <= bound+1; a++) {
-			    for (int b = 2; b <= bound+1; b++) {
+			for (int a = 2; a <= bound; a++) {
+			    for (int b = 2; b <= bound; b++) {
 			        double result = Math.pow(a, b);
 			        set.add(result);
 			     }
 			 }
-			System.out.println(set.size());
-	}
 			return set.size();
 		}
 
